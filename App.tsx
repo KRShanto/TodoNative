d9ea1a11d96d3a21b62a822488165ea24e1ed811
KR Shanto <84588948+KRShanto@users.noreply.github.com>
2022-12-12 01:03:31 +0600

    Add navigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// project:
+// A todo app with React Native
+// it will have a list of projects
+// and a list of todos for each project
+// users can add new projects and then view the projects in the home screen
+// users can add new todos and view them in the project screen
+// users can mark todos as complete
+// users can delete todos
+// users can delete projects
+// users can edit todos
+// users can edit projects
+// users can view the number of todos in each project
+// There also will be a about screen
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import Home from "./pages/Home";
+import Project from "./pages/Project";
+import { TodoProjectContext } from "./constants/contexts";
+import colors from "./constants/colors";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    const [projects, setProjects] = useState([
+        {
+            name: "Project 1",
+            todos: [
+                {
+                    name: "Todo 1",
+                    completed: false,
+                },
+                {
+                    name: "Todo 2",
+                    completed: false,
+                },
+            ],
+        },
+        {
+            name: "Project 2",
+            todos: [
+                {
+                    name: "Todo 1",
+                    completed: false,
+                },
+                {
+                    name: "Todo 2",
+                    completed: false,
+                },
+            ],
+        },
+    ]);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    useEffect(() => {
+        console.log("projects", projects);
+    }, [projects]);
+
+    return (
+        <TodoProjectContext.Provider value={{ projects, setProjects }}>
+            <NavigationContainer>
+                <Stack.Navigator
+                    screenOptions={{
+                        headerStyle: {
+                            backgroundColor: colors.navbar.bg,
+                        },
+                        headerTintColor: colors.navbar.text,
+                    }}
+                >
+                    <Stack.Screen
+                        name="Home"
+                        component={Home}
+                        options={{
+                            title: "Home",
+                        }}
+                    />
+                    <Stack.Screen
+                        name="Project"
+                        component={Project}
+                        options={({ route }) => ({
+                            // @ts-ignore
+                            title: route.params.projectName,
+                        })}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </TodoProjectContext.Provider>
+    );
+}
